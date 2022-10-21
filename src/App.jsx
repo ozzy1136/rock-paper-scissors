627c1711f77f20237ed21e01339008efd2d93f0d
Ozmar Mendoza <ozmarmen@gmail.com>
2022-10-20 22:22:08 -0500

    Incorporate Frontend Mentor starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -7,18 +6,6 @@ function App() {
 
 	return (
 		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
@@ -31,6 +18,16 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<footer>
+				<div className="attribution">
+					Challenge by{" "}
+					<a href="https://www.frontendmentor.io?ref=challenge">
+						Frontend Mentor
+					</a>
+					. Coded by{" "}
+					<a href="https://github.com/ozzy1136">Ozmar Mendoza</a>.
+				</div>
+			</footer>
 		</div>
 	);
 }
