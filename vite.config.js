ded186fed676b94f8624faa51d56e262163fb407
Ozmar Mendoza <amfc11@gmail.com>
2022-10-30 16:09:20 -0500

    Add aliases for assets and components

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
@@ -6,4 +7,10 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
 	plugins: [svgr(), react()],
 	base: "/rock-paper-scissors/",
+	resolve: {
+		alias: {
+			assets: path.resolve(__dirname, "src/assets"),
+			components: path.resolve(__dirname, "src/components"),
+		},
+	},
 });
