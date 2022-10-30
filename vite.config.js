6438b3a4e4074ff9be42f2901acdad41528a7c24
Ozmar Mendoza <amfc11@gmail.com>
2022-10-30 17:12:32 -0500

    Install vite-aliases

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { ViteAliases } from "vite-aliases";
 import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svgr(), react()],
+	plugins: [ViteAliases(), svgr(), react()],
 	base: "/rock-paper-scissors/",
-	resolve: {
-		alias: {
-			assets: path.resolve(__dirname, "src/assets"),
-			components: path.resolve(__dirname, "src/components"),
-		},
-	},
 });
